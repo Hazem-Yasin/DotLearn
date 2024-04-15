6d02b0210edcb7d09c9521d8295f1504d6e40111
Hazem-Yasin <hazem.yasin.1416@gmail.com>
2024-04-15 20:07:59 +0200

    will start over

--- DotLearn/DotLearn/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DotLearn.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
